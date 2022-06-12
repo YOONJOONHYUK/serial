7e4736da2a4261c69249269d06f290b2f397931d
YOONJOONHYUK <102521625+YOONJOONHYUK@users.noreply.github.com>
2022-06-13 04:43:57 +0900

    Add files via upload

---
+++ serial.ino
@@ -0,0 +1,10 @@
+void setup() 
+       {
+        //OPEN serial communications and
+        // wait for port to open:
+        Serial.begin(9600);
+        
+       Serial.println("Hello, world?");
+        }
+void loop() {Serial.println("안녕하세요");
+}
